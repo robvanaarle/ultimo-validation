d73610866b67e586917062f53fb086e1599d5091
Rob van Aarle <robvanaarle@gmail.com>
2014-02-25 20:36:26 +0100

    Added more tests

---
+++ tests/ultimo/validation/ValidatorTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace ultimo\validation;
+
+class ValidatorTest extends \PHPUnit_Framework_TestCase {
+  
+  public function testValidValueIsValidAndContainsNoErrors() {
+    $mock = $this->getMockForAbstractClass('\ultimo\validation\Validator');
+    $mock->expects($this->once())
+         ->method('valueIsValid')
+         ->with($this->equalTo('value_to_test'))
+         ->will($this->returnValue(true));
+    
+    $this->assertTrue($mock->isValid('value_to_test'));
+    $this->assertSame(array(), $mock->getErrors());
+  }
+  
+  public function testInvalidValueIsNotValidAndContainsErrors() {
+    $mock = $this->getMockForAbstractClass('\ultimo\validation\Validator');
+    $mock->expects($this->once())
+         ->method('valueIsValid')
+         ->with($this->equalTo('value_to_test'))
+         ->will($this->returnValue(false));
+    
+    $this->assertFalse($mock->isValid('value_to_test'));
+  }
+  
+  public function testSubclassesCanAddErrors() {
+    $mock = $this->getMockForAbstractClass('\ultimo\validation\Validator');
+    $mock->expects($this->once())
+         ->method('valueIsValid')
+         ->will($this->returnCallback(function() use ($mock) {
+           // It is bad practice to test protected functions, but addError() is
+           // a function especially made available to subclasses.
+           $class = new \ReflectionClass(get_class($mock));
+           $method = $class->getMethod('addError');
+           $method->setAccessible(true);
+           
+           $method->invokeArgs($mock, array('result_error'));
+           
+           return false;
+         }));   
+    $mock->isValid('value_to_test');
+         
+    $this->assertSame(array('result_error'), $mock->getErrors());
+  }
+  
+  public function testSubclassesCanAddAVariable() {
+    $mock = $this->getMockForAbstractClass('\ultimo\validation\Validator');
+    $mock->expects($this->once())
+         ->method('valueIsValid')
+         ->will($this->returnCallback(function() use ($mock) {
+           // It is bad practice to test protected functions, but setVariable() is
+           // a function especially made available to subclasses.
+           $class = new \ReflectionClass(get_class($mock));
+           $method = $class->getMethod('setVariable');
+           $method->setAccessible(true);
+           
+           $method->invokeArgs($mock, array('var_name', 42));
+           
+           return false;
+         }));
+    $mock->isValid('value_to_test');
+    
+    $this->assertSame(array('value' => 'value_to_test', 'var_name' => 42), $mock->getVariables());
+  }
+  
+  public function testSubclassesCanAddVariables() {
+    $mock = $this->getMockForAbstractClass('\ultimo\validation\Validator');
+    $mock->expects($this->once())
+         ->method('valueIsValid')
+         ->will($this->returnCallback(function() use ($mock) {
+           // It is bad practice to test protected functions, but setVariables() is
+           // a function especially made available to subclasses.
+           $class = new \ReflectionClass(get_class($mock));
+           $method = $class->getMethod('setVariables');
+           $method->setAccessible(true);
+           
+           $method->invokeArgs($mock, array(array('var_name' => 42)));
+           
+           return false;
+         }));
+    $mock->isValid('value_to_test');
+    
+    $this->assertSame(array('value' => 'value_to_test', 'var_name' => 42), $mock->getVariables());
+  }
+
+}
---
+++ tests/ultimo/validation/validators/EmailAddressTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ultimo\validation\validators;
+
+class EmailAddressTest extends \PHPUnit_Framework_TestCase {
+  protected $validator;
+  
+  public function setup() {
+    $this->validator = new EmailAddress();
+  }
+  
+  public function provideValidEmailAddresses() {
+    return array(
+      array('name@server.com'),
+      array('name.lastname@server.com'),
+      array('name@server.co.uk'),
+      array('name+label@server.com')
+    );
+  }
+  
+  /**
+   * @dataProvider provideValidEmailAddresses
+   */
+  public function testEmailAddressIsValid($emailAddress) {
+    $this->assertTrue($this->validator->isValid($emailAddress));
+    $this->assertSame(array(), $this->validator->getErrors());
+  }
+  
+  public function provideInvalidEmailAddresses() {
+    return array(
+      array('name@server'),
+      array('@server.com'),
+      array('name@server.c'),
+      array('name@weird@server.com')
+    );
+  }
+  
+  /**
+   * @dataProvider provideInvalidEmailAddresses
+   */
+  public function testEmailAddressIsInvalid($emailAddress) {
+    $this->assertFalse($this->validator->isValid($emailAddress));
+    $this->assertSame(array(EmailAddress::INVALID_EMAILADDRESS), $this->validator->getErrors());
+  }
+}
