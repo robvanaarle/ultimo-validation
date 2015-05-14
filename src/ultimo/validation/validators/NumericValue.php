5be01bd3f2afdca7c0855e3d04107877a7473f7b
Rob van Aarle <robvanaarle@gmail.com>
2015-05-14 09:08:40 +0200

    NumericValue now has option for whole numbers only

--- src/ultimo/validation/validators/NumericValue.php
+++ src/ultimo/validation/validators/NumericValue.php
@@ -6,19 +6,25 @@ class NumericValue extends \ultimo\validation\Validator {
   
   const TOO_LOW = 'too_low';
   const TOO_HIGH = 'too_high';
+  const NOT_WHOLE = 'not_whole';
   
   private $min;
   private $max;
+  private $whole;
   
-  public function __construct($min, $max=null) {
+  public function __construct($min, $max=null, $whole=false) {
     $this->min = $min;
     $this->max = $max;
+    $this->whole = $whole;
   }
   
   protected function valueIsValid($value) {
     $this->setVariables(array('min' => $this->min, 'max' => $this->max));
     
-    if ($this->min !== null && $value < $this->min) {
+    if ($this->whole && (is_int($value) || !preg_match("/^-?[0-9]+$/", $value))) {
+      $this->addError(self::NOT_WHOLE);
+      return false;
+    } if ($this->min !== null && $value < $this->min) {
       $this->addError(self::TOO_LOW);
       return false;
     } else if ($this->max !== null && $value > $this->max) {
